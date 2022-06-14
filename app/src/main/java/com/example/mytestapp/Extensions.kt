8cf709aacb3407656db7e771de5ef6c1e6413993
Abolfazl <a.ghanbari@mahsan.co>
2022-06-14 18:13:30 +0430

    init :)

---
+++ app/src/main/java/com/example/mytestapp/Extensions.kt
@@ -0,0 +1,6 @@
+package com.example.mytestapp
+
+
+class Extensions {
+
+}
---
+++ app/src/main/java/com/example/mytestapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.mytestapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mytestapp.ui.theme.MyTestAppTheme
+import com.parse.ParseObject
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyTestAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+
+        insertStudent("")
+    }
+
+    private fun insertStudent(name: String) {
+        val firstObject = ParseObject("Student")
+        firstObject.put("name", name)
+        firstObject.saveInBackground {
+            if (it != null) {
+                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+            } else {
+                Log.d("MainActivity", "Object saved.")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyTestAppTheme {
+        Greeting("Android")
+    }
+}
